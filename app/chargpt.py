5716a693077c5fa3f3d9b1c41157ca61c6456083
lvgalvaofilho <lvgalvaofilho@gmail.com>
2023-09-28 04:40:40 -0300

    update chatgpt, getdata, streamlit

---
+++ app/chargpt.py
@@ -0,0 +1,38 @@
+import openai
+from dotenv import load_dotenv
+import os
+
+# Carrega as variáveis de ambiente do arquivo .env
+load_dotenv()
+
+# Define suas credenciais de API
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def read_context_from_txt(filename="output.txt"):
+    """Lê o contexto do arquivo .txt fornecido."""
+    with open(filename, "r") as file:
+        return file.read()
+
+def ask_gpt(question, context):
+    """Faz uma pergunta ao modelo GPT da OpenAI com um contexto específico."""
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": context},
+            {"role": "user", "content": question}
+        ]
+    )
+    return response.choices[0].message['content'].strip()
+
+def main():
+    context = read_context_from_txt()
+    while True:
+        question = input("Digite sua pergunta (ou 'sair' para terminar): ")
+        if question.lower() == 'sair':
+            break
+        answer = ask_gpt(question, context)
+        print(f"Resposta: {answer}\n")
+
+if __name__ == "__main__":
+    main()
--- app/get_data.py
+++ app/get_data.py
@@ -1,32 +1,20 @@
-import asyncio
-
-import httpx
+import requests
 from bs4 import BeautifulSoup
-from icecream import ic
-
 
 def url_builder(termo: str) -> str:
     """
     Define a URL base e o termo de busca.
-
-    args:
-        termo (str): termo de busca
     """
     url_base = "https://portal.gupy.io/job-search/term="
     return f"{url_base}{termo}"
 
-
-# Função assíncrona para obter conteúdo da página
-async def fetch_page(url: str):
+def fetch_page(url: str):
     """
-    Obtém o conteúdo da página.
+    Obtém o conteúdo da página usando 'requests'.
     """
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url)
-        return response.content
+    response = requests.get(url)
+    return response.content
 
-
-# Função para extrair todos os hrefs
 def extract_hrefs(page_content):
     """
     Extrai todos os hrefs da página.
@@ -36,19 +24,47 @@ def extract_hrefs(page_content):
     hrefs = [link.get("href") for link in links if link.get("href") is not None]
     return hrefs
 
+def extract_body_text(url: str) -> str:
+    """
+    Extrai o texto do corpo da página de um URL.
+    """
+    page_content = fetch_page(url)
+    soup = BeautifulSoup(page_content, "html.parser")
+    # Removendo todos os scripts e styles do conteúdo
+    for script in soup(["script", "style"]):
+        script.extract()
+    # Pegando o texto do corpo da página
+    return soup.get_text()
 
-# Função principal
+def save_to_txt(text: str, filename="output.txt"):
+    """
+    Anexa o texto fornecido a um arquivo .txt.
+    """
+    with open(filename, "a") as file:
+        file.write(text + "\n\n")
+
 def main():
+    # Reiniciando o arquivo 'output.txt'
+    with open("output.txt", "w"):
+        pass
+
     termo = input("Digite o termo de busca: ")
     url = url_builder(termo)
-    page_content = asyncio.run(fetch_page(url))
+    page_content = fetch_page(url)
     hrefs = extract_hrefs(page_content)
+    
     if len(hrefs) == 1:
         print("Você deve digitar outro termo")
-    else:
-        for href in hrefs:
-            print(href)
+        return
 
+    hrefs = [href for href in hrefs if "://" in href][:2]  # Limitando a 5 hrefs válidos
+
+    for href in hrefs:
+        print(f"Processando {href}...")
+        body_text = extract_body_text(href)
+        save_to_txt(body_text)
+
+    print("Finalizado!")
 
 if __name__ == "__main__":
     main()
